7622efec64a122da7c56382610584976d877722d
Hendi Santika <hendisantika@gmail.com>
2020-01-02 07:03:27 +0700

    #1 - Initial Commit

---
+++ src/main/java/com/hendisantika/springbootfactorypattern/SpringbootFactoryPatternApplication.java
@@ -0,0 +1,13 @@
+package com.hendisantika.springbootfactorypattern;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootFactoryPatternApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootFactoryPatternApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hendisantika/springbootfactorypattern/SpringbootFactoryPatternApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hendisantika.springbootfactorypattern;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootFactoryPatternApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
